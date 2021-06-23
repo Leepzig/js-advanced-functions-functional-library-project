81be874b5a809011cf8e1183d135f41637187bf4
andrew <neon.duncan@gmail.com>
2021-06-23 16:22:38 -0500

    commit tree aada9881c8a1143b280b53b866683c1dca6ea97d

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, cb) {
+      if (Array.isArray(collection)) {
+        for (let i = 0; i < collection.length; i++) {
+          cb(collection[i], i, collection)
+        }
+      } else if ( typeof collection === "object") {
+        for (property in collection) {
+          cb(collection[property], property, collection)
+        }
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, cb) {
+      const newCollection = []
+      for (let i = 0; i < collection.length; i++) {
+        const newEl = cb(collection)
+        newCollection.push(newEl)
+      }
+      return newCollection
     },
 
     reduce: function() {
